fc26eed699b28bc818457cbb0efc6ab161f41263
lokeshh <lokeshhsharma@gmail.com>
2016-10-22 12:17:25 +0000

    add admin and user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  get 'admin', to: 'admin#show', as: :admin
+  get 'user', to: 'user#show', as: :user
 end
